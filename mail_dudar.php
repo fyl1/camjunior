4e4fa90580204f5b3cf2b2ccc35231393bd7cf7c
97fyl97@gmail.com <97fyl97@gmail.com>
2020-07-01 15:00:58 +0300

    none push maill

--- mail_dudar.php
+++ mail_dudar.php
@@ -8,7 +8,7 @@ $text = ' Имя: '.$name.'  Телефон: '.$phone.'  Текст: '.$comm;
 
 $headers = "From: camjunior.ru\r\nReply-to: $phone\r\nContent-type: text; charset=utf-8\r\n";
 
-$success = mail('orlovbogdan1998@gmail.com, redhope085@gmail.com', $headers, $text);
+
 // $success = mail('Dizilll@mail.ru, reg_nik@icloud.com, Dizilll1@yandex.ru', $headers, $text);
 
 ?>
